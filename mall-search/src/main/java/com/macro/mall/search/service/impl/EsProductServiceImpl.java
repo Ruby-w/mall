5ae824b5bb1891a1f0a033d743626d672853b2e0
healer <15294610166@163.com>
2020-08-04 00:03:18 +0800

    品牌信息> 完成！！！

---
+++ mall-search/src/main/java/com/macro/mall/search/service/impl/EsProductServiceImpl.java
@@ -0,0 +1,289 @@
+package com.macro.mall.search.service.impl;
+
+import com.macro.mall.search.dao.EsProductDao;
+import com.macro.mall.search.domain.EsProduct;
+import com.macro.mall.search.domain.EsProductRelatedInfo;
+import com.macro.mall.search.reposiory.EsProductRepository;
+import com.macro.mall.search.service.EsProductService;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.lucene.search.function.FunctionScoreQuery;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.filter.InternalFilter;
+import org.elasticsearch.search.aggregations.bucket.nested.InternalNested;
+import org.elasticsearch.search.aggregations.bucket.terms.LongTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Score;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName EsProductServiceImpl
+ * @Description
+ * @Author AW
+ * @Date 2020/8/2 000222:05
+ * @Version V1.0
+ **/
+@Service
+public class EsProductServiceImpl implements EsProductService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EsProductServiceImpl.class);
+
+
+    @Autowired
+    private EsProductDao productDao;
+    @Autowired
+    private EsProductRepository productRepository;
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Override
+    public int importAll() {
+        List<EsProduct> productList = productDao.getAllEsProductList(null);
+        Iterable<EsProduct> esProductIterable = productRepository.saveAll(productList);
+        Iterator<EsProduct> iterator = esProductIterable.iterator();
+        int result = 0;
+        while (iterator.hasNext()) {
+            result++;
+            iterator.next();
+        }
+        return result;
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public EsProduct create(Long id) {
+        EsProduct result = null;
+        List<EsProduct> productList = productDao.getAllEsProductList(id);
+        if (productList.size() > 0) {
+            EsProduct product = productList.get(0);
+            result = productRepository.save(product);
+        }
+
+        return result;
+    }
+
+    @Override
+    public void delete(List<Long> ids) {
+        if (!CollectionUtils.isEmpty(ids)) {
+            List<EsProduct> list = new ArrayList<>();
+            for (Long id : ids) {
+                EsProduct product = new EsProduct();
+                product.setId(id);
+                list.add(product);
+            }
+            productRepository.deleteAll(list);
+        }
+    }
+
+    @Override
+    public Page<EsProduct> search(String keyword, Integer pageNum, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        return productRepository.findByNameOrSubTitleOrKeywords(keyword, keyword, keyword, pageable);
+    }
+
+    @Override
+    public Page<EsProduct> search(String keyword, Long brandId, Long productCategoryId, Integer pageNum, Integer pageSize, Integer sort) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        // 分页
+        builder.withPageable(pageable);
+        //过滤
+        if (brandId != null || productCategoryId != null) {
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+            if (brandId != null) {
+                boolQueryBuilder.must(QueryBuilders.termQuery("brandId", brandId));
+            }
+            if (productCategoryId != null) {
+                boolQueryBuilder.must(QueryBuilders.termQuery("productCategoryId", productCategoryId));
+            }
+            builder.withFilter(boolQueryBuilder);
+        }
+        //搜索
+        if (StringUtils.isEmpty(keyword)) {
+            builder.withQuery(QueryBuilders.matchAllQuery());
+        } else {
+            List<FunctionScoreQueryBuilder.FilterFunctionBuilder> filterFunctionBuilders = new ArrayList<>();
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("name", keyword),
+                    ScoreFunctionBuilders.weightFactorFunction(10)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("subTitle", keyword),
+                    ScoreFunctionBuilders.weightFactorFunction(5)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("keywords", keyword),
+                    ScoreFunctionBuilders.weightFactorFunction(2)));
+            FunctionScoreQueryBuilder.FilterFunctionBuilder[] builders = new FunctionScoreQueryBuilder.FilterFunctionBuilder[filterFunctionBuilders.size()];
+            filterFunctionBuilders.toArray(builders);
+            FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(builders)
+                    .scoreMode(FunctionScoreQuery.ScoreMode.SUM)
+                    .setMinScore(2);
+            builder.withQuery(functionScoreQueryBuilder);
+        }
+        //排序
+        if (sort == 1) {
+            //按新品从新到旧
+            builder.withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC));
+        } else if (sort == 2) {
+            // 按销量从高到低
+            builder.withSort(SortBuilders.fieldSort("id").order(SortOrder.DESC));
+        } else if (sort == 3) {
+            // 按价格 从低到高
+            builder.withSort(SortBuilders.fieldSort("price").order(SortOrder.ASC));
+        } else if (sort == 4) {
+            //按价格 从高到低
+            builder.withSort(SortBuilders.fieldSort("price").order(SortOrder.DESC));
+        } else {
+            //按相关度
+            builder.withSort(SortBuilders.scoreSort().order(SortOrder.DESC));
+        }
+        builder.withSort(SortBuilders.scoreSort().order(SortOrder.DESC));
+        NativeSearchQuery searchQuery = builder.build();
+        LOGGER.info("DSL:{}", searchQuery.getQuery().toString());
+        return productRepository.search(searchQuery);
+    }
+
+    @Override
+    public Page<EsProduct> recommend(Long id, Integer pageNum, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        List<EsProduct> productList = productDao.getAllEsProductList(id);
+        if (productList.size() > 0) {
+            EsProduct esProduct = productList.get(0);
+            String keyword = esProduct.getName();
+            Long brandId = esProduct.getBrandId();
+            Long productCategoryId = esProduct.getProductCategoryId();
+            //根据商品标签、品牌、分类进行搜索
+            List<FunctionScoreQueryBuilder.FilterFunctionBuilder> filterFunctionBuilders = new ArrayList<>();
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("name", keyword), ScoreFunctionBuilders.weightFactorFunction(8)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("subTitle", keyword),
+                    ScoreFunctionBuilders.weightFactorFunction(2)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("keywords", keyword),
+                    ScoreFunctionBuilders.weightFactorFunction(2)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("brandId", brandId),
+                    ScoreFunctionBuilders.weightFactorFunction(5)));
+            filterFunctionBuilders.add(new FunctionScoreQueryBuilder.FilterFunctionBuilder(QueryBuilders.matchQuery("productCategoryId", productCategoryId),
+                    ScoreFunctionBuilders.weightFactorFunction(3)));
+            FunctionScoreQueryBuilder.FilterFunctionBuilder[] builders = new FunctionScoreQueryBuilder.FilterFunctionBuilder[filterFunctionBuilders.size()];
+            filterFunctionBuilders.toArray(builders);
+            FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(builders)
+                    .scoreMode(FunctionScoreQuery.ScoreMode.SUM)
+                    .setMinScore(2);
+            //用于过滤相同的商品
+            BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+            boolQueryBuilder.mustNot(QueryBuilders.termQuery("id", id));
+            //构建查询条件
+            NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+            builder.withQuery(functionScoreQueryBuilder);
+            builder.withFilter(boolQueryBuilder);
+            builder.withPageable(pageable);
+            NativeSearchQuery searchQuery = builder.build();
+            LOGGER.info("DSL:{}", searchQuery.getQuery().toString());
+            return productRepository.search(searchQuery);
+        }
+        return new PageImpl<>(null);
+
+
+    }
+
+    @Override
+    public EsProductRelatedInfo searchRelatedInfo(String keyword){
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        //搜索条件
+if(StringUtils.isEmpty(keyword)){
+builder.withQuery(QueryBuilders.matchAllQuery());
+}else{
+builder.withQuery(QueryBuilders.multiMatchQuery(keyword,"name","subTitle","keyword"));
+}
+        // 聚合搜索品牌名称
+builder.addAggregation(AggregationBuilders.terms("brandNames").field("brandName"));
+       //集合搜索分类名称
+builder.addAggregation(AggregationBuilders.terms("productCategoryNames").field("productCategoryNames"));
+        //聚合搜索商品属性，去除type=1的属性
+        AbstractAggregationBuilder aggregationBuilder = AggregationBuilders.nested("allAttrValues","attrValueList")
+                .subAggregation(AggregationBuilders.filter("productAttrs",QueryBuilders.termQuery("attrValueList.type",1))
+                        .subAggregation(AggregationBuilders.terms("attrIds")
+                                .field("attrValueList.productAttributeId")
+                                .subAggregation(AggregationBuilders.terms("attrValues")
+                                        .field("attrValueList.value"))
+                                .subAggregation(AggregationBuilders.terms("attrNames")
+                                        .field("attrValueList.name"))));
+        builder.addAggregation(aggregationBuilder);
+        NativeSearchQuery searchQuery = builder.build();
+        return elasticsearchTemplate.query(searchQuery, response -> {
+            LOGGER.info("DSL:{}",searchQuery.getQuery().toString());
+            return convertProductRelatedInfo(response);
+        });
+    }
+    /**
+     * 将返回结果转换为对象
+     */
+    private EsProductRelatedInfo convertProductRelatedInfo(SearchResponse response) {
+        EsProductRelatedInfo productRelatedInfo = new EsProductRelatedInfo();
+        Map<String, Aggregation> aggregationMap = response.getAggregations().getAsMap();
+        //设置品牌
+        Aggregation brandNames = aggregationMap.get("brandNames");
+        List<String> brandNameList = new ArrayList<>();
+        for(int i = 0; i<((Terms) brandNames).getBuckets().size(); i++){
+            brandNameList.add(((Terms) brandNames).getBuckets().get(i).getKeyAsString());
+        }
+        productRelatedInfo.setBrandNames(brandNameList);
+        //设置分类
+        Aggregation productCategoryNames = aggregationMap.get("productCategoryNames");
+        List<String> productCategoryNameList = new ArrayList<>();
+        for(int i=0;i<((Terms) productCategoryNames).getBuckets().size();i++){
+            productCategoryNameList.add(((Terms) productCategoryNames).getBuckets().get(i).getKeyAsString());
+        }
+        productRelatedInfo.setProductCategoryNames(productCategoryNameList);
+        //设置参数
+        Aggregation productAttrs = aggregationMap.get("allAttrValues");
+        List<LongTerms.Bucket> attrIds = ((LongTerms) ((InternalFilter) ((InternalNested) productAttrs).getProperty("productAttrs")).getProperty("attrIds")).getBuckets();
+        List<EsProductRelatedInfo.ProductAttr> attrList = new ArrayList<>();
+        for (Terms.Bucket attrId : attrIds) {
+            EsProductRelatedInfo.ProductAttr attr = new EsProductRelatedInfo.ProductAttr();
+            attr.setAttrId((Long) attrId.getKey());
+            List<String> attrValueList = new ArrayList<>();
+            List<StringTerms.Bucket> attrValues = ((StringTerms) attrId.getAggregations().get("attrValues")).getBuckets();
+            List<StringTerms.Bucket> attrNames = ((StringTerms) attrId.getAggregations().get("attrNames")).getBuckets();
+            for (Terms.Bucket attrValue : attrValues) {
+                attrValueList.add(attrValue.getKeyAsString());
+            }
+            attr.setAttrValues(attrValueList);
+            if(!CollectionUtils.isEmpty(attrNames)){
+                String attrName = attrNames.get(0).getKeyAsString();
+                attr.setAttrName(attrName);
+            }
+            attrList.add(attr);
+        }
+        productRelatedInfo.setProductAttrs(attrList);
+        return productRelatedInfo;
+    }
+}
